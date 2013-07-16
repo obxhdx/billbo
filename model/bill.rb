ce6cd3e5c123f4d3644f78abb2d31d0e96bcd8c0
obxhdx <obxhdx@yahoo.com>
2013-07-16 20:29:23 -0300

    Remove uneeded Bill methods and tests

--- model/bill.rb
+++ model/bill.rb
@@ -14,16 +14,4 @@ class Bill
   def to_s
     "Bill: issued_by:#{self.issued_by}, due_date:#{self.due_date}, total_amount:#{self.total_amount}, barcode:#{self.barcode}, status:#{self.status}"
   end
-
-  def ==(other_bill)
-    self.attributes.except('_id').eql? other_bill.attributes.except('_id')
-  end
-
-  def to_hash
-    hash = {}
-    instance_variables.each do
-      |var| hash[var.to_s.delete("@")] = instance_variable_get(var)
-    end
-    hash
-  end
 end
