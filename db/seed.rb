5a3831ef9ed05fe47a76194be9e2a8f6f19e516f
obxhdx <obxhdx@yahoo.com>
2013-07-16 20:46:07 -0300

    Fix seed issue

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,3 @@
-3.times {
-  Bill.create(
-    issued_by:    'company1',
-    due_date:     '25-02-2013',
-    total_amount: 45.9,
-    barcode:      '0000000000000000000000001')
-}
+Bill.create(issued_by: 'company1', due_date: '25-02-2013', total_amount: 45.9, barcode: '0000000000000000000000001')
+Bill.create(issued_by: 'company2', due_date: '28-02-2013', total_amount: 132.32, barcode: '0000000000000000000000002')
+Bill.create(issued_by: 'company3', due_date: '25-04-2013', total_amount: 72.0, barcode: '0000000000000000000000003')
